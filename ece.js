ffede696b10c105845a8916f326b11f0136c109c
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-06 17:35:18 +0300

    first commit

---
+++ ece.js
@@ -0,0 +1,13 @@
+const sifreniz = "sjdhfs!shfdk";
+if (sifreniz.length>= 12 &&  sifreniz.includes("!"))
+{
+    console.log("sifrem baya guclu");
+}
+
+else{
+console.log("sifreyi yeniden giriniz");
+}
+
+const control=false;
+if(!control)
+{console.log("kontrol basarili");}
